2408797c9c466dbf5d24a61f335498bc9604a734
SavyParchman <savyparchman@gmail.com>
2017-09-19 15:32:39 -0500

    C0m9L3t3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+
+const messages = [
+  {
+    message: "savy",
+    age: 26
+  },
+  { 
+    message: "hello",
+    age: 1
+}];
+
+
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
+});
